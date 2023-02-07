57d453e01b35621fd27760bea32d42ca43f20163
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2023-02-07 10:12:52 +0800

    Add files via upload

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -5,12 +5,11 @@ mkdir $moduledir/system/bin
 mv $moduledir/gameoptd  $moduledir/system/bin
 chmod 755 $moduledir/system/bin/gameoptd
 )
-
-[[ ! -d $moduledir/toggle ]] && (
-mkdir $moduledir/toggle
-touch $moduledir/toggle/enable_gpu_tune
+[[ ! -d $moduledir/toggler ]] && (
+mkdir $moduledir/toggler
+echo "0" > $moduledir/toggler/enable_gpu_tune
+echo "1" > $moduledir/toggler/enable_notification
 )
-
 [[ ! -d $moduledir/log ]] && (
 mkdir $moduledir/log
 )
--- service.sh
+++ service.sh
@@ -1,3 +1,4 @@
+moduledir="/data/adb/modules/gameoptd"
 while [[ -z $(getprop sys.boot_completed) ]]; do sleep 5; done
-gameoptd
-su -lp 2000 -c "cmd notification post -S bigtext -t 'Game Optimization Daemon' tag 'Running correctly"
+gameoptd &
+[[ "$(cat "$moduledir/toggler/enable_notification")" == "1" ]] && (su -lp 2000 -c "cmd notification post -S bigtext -t 'Game Optimization Daemon' tag 'Running correctly'")
